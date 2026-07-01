ed4fe21a7b920143dd5a845d5dbb278317f816d0
Константин Вент <kostiavent@yandex.ru>
2026-07-02 02:21:40 +0300

    Создание структуры проекта и установка зависимостей

---
+++ main.py
@@ -0,0 +1,15 @@
+import sys
+
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+def main():
+    app = QApplication(sys.argv)
+    window = QMainWindow()
+    window.setWindowTitle("Trading Sandbox")
+    window.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
